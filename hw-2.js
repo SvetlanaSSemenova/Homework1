82b90d5571dee869d3b7dd314876c58b2375d0f5
SvetlanaSSemenova <sveta.pav@list.ru>
2024-09-24 17:20:24 +0300

    first commit

---
+++ hw-2.js
@@ -0,0 +1,53 @@
+// Задание 1
+let a = 10;
+a = 20;
+alert (a);
+
+// Задание 2
+let year = "Дата выпуска первого iPhone - 29.06.2007"
+alert (year);
+
+// Задание 3
+let eich = "JavaScript создал Брендан Эйх."
+alert (eich);
+
+// Задание 4
+let b = 10;
+let c = 2;
+let result = b + c;
+alert (result);
+
+// Задание 5
+let power = 2 ** 5;
+alert (power);
+
+// Задание 6
+let d = 9;
+let i = 2;
+let remainder = d % i;
+alert (remainder);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert (age);
+
+// Задание 9
+const user = {
+    name: 'Николай',
+    age: 40,
+    isAdmin: false
+  };
+
+  // Задание 10
+    let userName = prompt('Как вас зовут?');
+    alert(`Привет, ${userName}!`);
